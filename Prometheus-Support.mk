7389b8e2ef6b0f23a1d483feaf3446ca2f6557c3
f18m <francesco.montorsi@gmail.com>
2024-05-28 11:37:18 +0200

    f

--- Prometheus-Support.mk
+++ Prometheus-Support.mk
@@ -8,15 +8,15 @@ ifeq ($(PROMETHEUS_SUPPORT),1)
 
 $(info INFO: Prometheus support is ENABLED)
 
-ifeq ($(wildcard $(ROOT_DIR)/conanbuildinfo.mak),)
+ifeq ($(wildcard $(ROOT_DIR)/conandeps.mk),)
 $(error Please run 'conan install . --build=missing' first)
 endif
 
 # include prometheus-cpp depedency through the GNU make variables provided by Conan at install time
 # NOTE: for some reason "dl pthread" libraries must be explicitly listed
-include $(ROOT_DIR)/conanbuildinfo.mak
+include $(ROOT_DIR)/conandeps.mk
 LIBS += $(foreach libdir,$(CONAN_LIB_DIRS),-L$(libdir)) $(foreach lib,$(CONAN_LIBS),-l$(lib)) -ldl -lpthread
-CXXFLAGS += $(CONAN_CXXFLAGS) -I$(CONAN_INCLUDE_DIRS_PROMETHEUS-CPP)
+CXXFLAGS += $(CONAN_CXXFLAGS) -I$(CONAN_INCLUDE_DIRS_PROMETHEUS_CPP)
 DEFS += -DPROMETHEUS_SUPPORT=1
 else
 
